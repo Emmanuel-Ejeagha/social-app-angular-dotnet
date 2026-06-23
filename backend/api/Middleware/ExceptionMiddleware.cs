49e3c7e94ea0e40535ed23a74bcd4e57290f57fb
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-23 19:47:02 +0100

    feat(api): add exception handling middleware

---
+++ backend/api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using System.Text.Json;
+
+namespace api.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware>  _logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException valEx)
+        {
+            _logger.LogWarning(valEx, "Validation failed");
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var errors = valEx.Errors
+                .GroupBy(e => e.PropertyName)
+                .ToDictionary(g => g.Key, g => g.Select(e => e.ErrorMessage).ToArray());
+
+            var response = new
+            {
+                message = "Validation failed",
+                errors
+            };
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unhandled exception occurred");
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var response = new { message = "An internal server error occurred." };
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
